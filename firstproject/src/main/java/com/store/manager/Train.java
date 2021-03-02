aff99640115d64ebc1ac73ad5ce9d119a766b6da
Boufnichel <mohamed@boufnichel.com>
2021-03-02 17:12:45 +0100

    TDD

---
+++ firstproject/src/main/java/com/store/manager/Train.java
@@ -0,0 +1,20 @@
+package com.store.manager;
+
+public class Train {
+    public Train(String hpp) {
+    }
+
+    public String print() {
+        return null;
+    }
+
+    public void detachEnd() {
+    }
+
+    public void detachHead() {
+    }
+
+    public boolean fill() {
+        return false;
+    }
+}
