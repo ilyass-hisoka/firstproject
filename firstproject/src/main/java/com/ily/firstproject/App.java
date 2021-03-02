d1f29bc734c26e8848dc720b910d74501b9e29a9
ilyass <eude1986@gmail.com>
2021-03-02 14:44:28 +0100

    test stream in App class

--- firstproject/src/main/java/com/ily/firstproject/App.java
+++ firstproject/src/main/java/com/ily/firstproject/App.java
@@ -1,15 +1,23 @@
 package com.ily.firstproject;
 
-import java.util.Optional;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.*;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        List<String> list = new ArrayList<String>();
+        
+        list.add("ilyass");
+        list.add("jawad");
+        list.add("simo");
+        
+        List<String> list2 = list.stream().filter(t -> t.equals("ilyass")).collect(Collectors.toList());
+        
+        list2.stream().forEach(System.out::println);
     }
 }
